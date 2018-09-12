af6bc2b430edc5cd8ffe2e39acdd102522e0b8fc
tatyana-andreichyk <tatyana.andreichyk@gmail.com>
2018-09-12 16:38:17 +0300

    love-triangle/feature: implemented calculation how many triangles in array

--- src/index.js
+++ src/index.js
@@ -3,5 +3,22 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    
+    let countTriangles = 0;
+    for (let n = 0; n <= preferences.length-1; n++) {
+        let k1 = preferences[n];
+        let k2 = preferences[k1 - 1];
+        let k3 = preferences[k2 - 1];
+        
+        if(k3 === n + 1){
+            countTriangles = countTriangles + 1;
+        }
+    }
+    
+    if (countTriangles > 0) {
+        countTriangles = parseInt(countTriangles / 3);
+    }
+    
+    return countTriangles;
+    
 };
